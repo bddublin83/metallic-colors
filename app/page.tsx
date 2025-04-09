c2b6de7747867a5f72a3601c58a90eca100d8100
bddublin83 <bddublin83@gmail.com>
2025-04-09 11:17:09 -0400

    Create home page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+app/page.tsx
+import CompleteLandingPage from "@/complete-landing-page"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <CompleteLandingPage />
+    </main>
+  )
+}
